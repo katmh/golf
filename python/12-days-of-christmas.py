b904e0c1b1c91c37f914175afede89833cda7033
Katherine Huang <hello@katmh.com>
2021-01-31 17:03:26 -0500

    Add 12 Days of Christmas Python

---
+++ python/12-days-of-christmas.py
@@ -0,0 +1,2 @@
+s=["Two Turtle Doves, and","Three French Hens,","Four Calling Birds,","Five Gold Rings,","Six Geese-a-Laying,","Seven Swans-a-Swimming,","Eight Maids-a-Milking,","Nine Ladies Dancing,","Ten Lords-a-Leaping,","Eleven Pipers Piping,","Twelve Drummers Drumming,"];c=["First","Second","Third","Fourth","Fifth","Sixth","Seventh","Eighth","Ninth","Tenth","Eleventh","Twelfth"]
+for i in range(12):print(*[f"On the {c[i]} day of Christmas\nMy true love sent to me",*reversed(s[:i]),"A Partridge in a Pear Tree.\n"],sep="\n")
